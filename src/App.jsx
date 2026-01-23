38d7c7b3e1649b461dfd3dcafb8632071c9f249c
Alan Webb <webbalan91939@gmail.com>
2026-01-23 13:21:41 +0000

    Create Pricing component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Banner from "./components/Banner";
-import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
 
 const App = () => {
 	return (
 		<div>
 			<Hero />
 			<Banner />
+			<Pricing />
 			<Footer />
 		</div>
 	);
